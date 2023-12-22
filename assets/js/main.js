a574fcdd41da6452d71a6fe3ce128b98f4b36a4b
philipkuerzel <philipkuerzel@gmail.com>
2023-12-22 13:24:27 +0100

    html done

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+const boxes = document.querySelectorAll(".box")
+const output = document.querySelector(".output")
+const reset = document.querySelector("button")
+const guessingColor = document.querySelector("h1")
+let colors = []
+
+const getRandomColor = () => {
+    
+}
